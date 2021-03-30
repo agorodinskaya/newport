ebf60b639f321b2c2e8089eafebdf8f7e8a4858d
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2021-03-31 10:15:48 +1100

    require path gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const path = require("path");
 module.exports = {
   siteMetadata: {
     title: "agportfolio",
-    siteUrl: "gifted-hypatia-8dde51.netlify.app",
+    siteUrl: "https://gifted-hypatia-8dde51.netlify.app",
   },
   plugins: [
     `gatsby-transformer-sharp`,
